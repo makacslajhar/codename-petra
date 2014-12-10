fb072bd210056dcf670fbaf3b330c108711c5291
Kakócz Erik <ke910720@sourceforge.net>
2014-12-10 20:23:28 +0100

    petra.cpp hozzaadva

---
+++ petra.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <osmium/io/any_input.hpp>
+#include <osmium/handler.hpp>
+#include <osmium/visitor.hpp>
+#include <osmium/osm/node.hpp>
+#include <osmium/osm/way.hpp>
+#include <osmium/osm/relation.hpp>
+#include <osmium/index/map/sparse_table.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+
+//Na ebből kéne kiindulni :D
+
+int main()
+{
+    std::cout<<"Hello World";
+    return 0;
+}
