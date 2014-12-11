39d46ef85bf1ca427696989004dade700a7e4a35
Kakócz Erik <ke910720@sourceforge.net>
2014-12-11 13:25:52 +0100

    install.sh módosítva

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo apt-get install freetype-*
+sudo apt-get install python-nose
 git clone https://github.com/mapnik/mapnik mapnik-2.3.x -b 2.3.x
 cd mapnik-2.3.x
+
 ./configure && make && sudo make install
