9302348bc111483ddbe8c3584df78fe3d32483d1
Kakócz Erik <ke910720@sourceforge.net>
2014-12-13 11:00:41 +0100

    módosítások az útkeresésen + a program mostmár négy darab argumentumot igényel a futáshoz. hastag tökömkivan

--- petra.cpp
+++ petra.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 //#include <cstddef>
 #include <osmium/io/xml_input.hpp>
@@ -10,41 +11,30 @@
 #include <osmium/handler/node_locations_for_ways.hpp>
 
 //Na ebből kéne kiindulni :D
+//ez még nem biztos hogy kell
+/*
+typedef struct
+{
+    osmium::Node csp;
+    osmium::Node * forras;
+}RNODE;
 
-array<osmium::Node> szurke;
-array<osmium::Node> feher;
+std::vector<osmium::Node> szurke;
+std::vector<osmium::Node> feher;
+*/
 
 /*Laci ezekbe kérem majd az utakat(<way></way>) illetve node-okat(<node></node>) beolvasni - Erik*/
-array<osmium::Way> utak;
-array<osmium::Node> nodeok;
+
+std::vector<osmium::Way> utak;
+std::vector<osmium::Node> nodeok;
 
 
-array<osmium::Node> connections(osmium::Node v_node)
-{
-    array<osmium::Node> res;
-    for(ut:utak)
-    {
-        bool benne_van=false;
-        for(n:ut.nodes())
-        {
-            if (n==v_node) benne_van=true;
-        }
-        if (benne_van)
-            for(n:ut.nodes())
-            {
-                res.add(n);
-            }
-    }
-    return res;
-}
-
-
-
-array<osmium::Way> dijkstra(osmium::Node Starting,osmium::Node Ending){
-    feher.add(Starting)
-
+std::vector<osmium::Way> route(osmium::Node Starting,osmium::Node Ending){
+    feher.push_back(Starting);
+    osmium::Node * cur;
     while (true){
-        for(n:szurke)
+
+        for(osmium::Node n:feher)
         {
 
         }
@@ -53,8 +43,21 @@ array<osmium::Way> dijkstra(osmium::Node Starting,osmium::Node Ending){
 }
 
 
-int main()
+void usage()
 {
-    std::cout<<"Hello World\n";
+    std::cout<<"usage: ./petra lat1 lon1 lat2 lon2"<<std::endl;
+}
+
+int main(int argc,const char * argv[])
+{
+    if (argc==5)
+    {
+        std::cout<<argv[1]<<' '<<argv[2]<<' '<<argv[3]<<' '<<argv[4]<<std::endl;
+    }
+    else
+    {
+        std::cout<<"nem megfelelo szamu koordinata!"<<std::endl;
+        usage();
+    }
     return 0;
 }
