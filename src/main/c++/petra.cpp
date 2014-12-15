c00e02734475b493ee1c018b75ae54ad06876a90
Kakócz Erik <ke910720@sourceforge.net>
2014-12-15 13:00:29 +0100

    asd

--- src/main/c++/petra.cpp
+++ src/main/c++/petra.cpp
@@ -74,8 +74,8 @@ int edge_number=tmp.size();
 Edge * Edge_array = &tmp[0];
 int * weights = &sulyok(edge_number)[0];
 
-NodeRefGraph g(Edge_array,Edge_array+edge_number,weights,node_num);
-property_map<NodeRefGraph,vertex_name_t>::type weightmap = get(vertex_name_t,g);
+//NodeRefGraph g(Edge_array,Edge_array+edge_number,weights,node_num);
+//property_map<NodeRefGraph,vertex_name_t>::type weightmap = get(vertex_name_t,g);
 
 //boost::dijkstra_shortest_paths(g)
 
